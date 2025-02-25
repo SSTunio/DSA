75a32c0614ad783e84e382f0c5c9fb396433e75e
SSTunio <2k21-it-198@student.sau.edu.pk>
2025-02-25 22:29:55 +0500

    add some variables

--- hello.java
+++ hello.java
@@ -2,5 +2,9 @@ public class hello{
 
     public static void main(String[] args) {
         System.out.print("hello world");
+        int age =20;
+        System.out.println("my age is "+age);
+        String name="sourih sindhi";
+        System.out.println("my name is "+ name);
     }
 }
